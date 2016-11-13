ced8c592f1a63a1ebb7d12fd06265f2a1ccca54f
R5meason <melynda.eason@gmail.com>
2016-11-13 12:06:53 -0800

    Adding skeletons

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Melynda Eason on 11/13/2016.
+ */
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCL2MDj_c4oPiHX4seu6TSQeuuEi51NjJk",
+    authDomain: "vapedata.firebaseapp.com",
+    databaseURL: "https://vapedata.firebaseio.com",
+    storageBucket: "vapedata.appspot.com",
+    messagingSenderId: "988632448397"
+};
+firebase.initializeApp(config);
+
+
